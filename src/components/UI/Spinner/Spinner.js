9201193ffdfab745a84050a7ec8ebcba571a9fe4
alexbursak <alexbursak3@gmail.com>
2020-05-03 12:35:28 +0100

    lesson 181

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classes from "./Spinner.css";
+
+// https://projects.lukehaas.me/css-loaders/
+const Spinner = (props) => (
+    <div className={classes.Spinner}>Loading...</div>
+);
+
+export default Spinner;
